d5e65dfa545a242f700a521fa2912310d09d5931
ibrohimjonadhamov22 <ibrohimjonadhamov22@gmail.com>
2025-11-13 14:20:54 +0900

    ad customer

--- main.py
+++ main.py
@@ -85,4 +85,99 @@ class Account:
 class Account:
     pass
 
+#### Ibrohimjon's Code
+
+class Customer:
+    def __init__(self, customer_id: str, name: str, email: str, phone: int, address: str, accounts_list: list):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.accounts_list = accounts_list
+
+        def add_account(self):
+            pass
+
+        def remove_account(self):
+            pass
+
+        def get_total_balance(self):
+            pass
+
+
+class IndividualAccount(Customer):
+    def __init__(self, customer_id: str, name: str, email: str, phone: int, address: str, accounts_list: list, date_of_birth, SSN, employment_status):
+        super().__init__(customer_id, name, email, phone, address, accounts_list)
+        self.date_of_birth = date_of_birth
+        self.SSN = SSN
+        self.employment_status = employment_status
+
+    def credit_score_calculation(self):
+        pass
+
+    def personal_lone_eligibility(self):
+        pass
+
+class CorporateAccount(Customer):
+    def __init__(self, customer_id: str, name: str, email: str, phone: int, address: str, accounts_list: list, company_name, tax_id, business_type, num_of_employees):
+        super().__init__(customer_id, name, email, phone, address, accounts_list)
+        self.company_name = company_name
+        self.tax_id = tax_id
+        self.business_type = business_type
+        self.num_of_employees = num_of_employees
+
+    def business_loan_eligibility(self):
+        pass
+
+    def bulk_transactions(self):
+        pass
+
+
+class Transaction:
+    def __init__(self, transaction_id, timestamp, amount, transaction_type, from_account, to_account):
+        self.transaction_id = transaction_id
+        self.timestamp = timestamp
+        self.amount = amount
+        self.transaction_type = transaction_type
+        self.from_account = from_account
+        self.to_account = to_account
+
+    def execute(self):
+        pass
+
+    def reverse(self):
+        pass
+
+    def validate(self):
+        pass
+
+class Deposit(Transaction):
+    def __init__(self, transaction_id, timestamp, amount, transaction_type, from_account, to_account, deposit_method):
+        super().__init__(transaction_id, timestamp, amount, transaction_type, from_account, to_account)
+        self.deposit_method = deposit_method
+
+    def execute(self):
+        pass
+    """adds to balance """
+
+class Withdraw(Transaction):
+    def __init__(self, transaction_id, timestamp, amount, transaction_type, from_account, to_account, withdrawal_method):
+        super().__init__(transaction_id, timestamp, amount, transaction_type, from_account, to_account)
+        self.deposit_method = withdrawal_method
+
+    def execute(self):
+        pass
+    """ deducts from balance with validation"""
+
+class Transfer(Transaction):
+    def __init__(self, transaction_id, timestamp, amount, transaction_type, from_account, to_account, source_account, destination_account, transfer_fee):
+        super().__init__(transaction_id, timestamp, amount, transaction_type, from_account, to_account)
+        self.source_account = source_account
+        self.destination_account = destination_account
+        self.transfer_fee = transfer_fee
+
+    def execute(self):
+        pass
+    """Moves money between accounts"""
 
